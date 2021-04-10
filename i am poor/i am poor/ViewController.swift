4b5f934e0d90acc748e0de8048631255c3a20548
pearlmalt <69867561+pearlmalt@users.noreply.github.com>
2021-04-10 16:55:03 -0700

    first commit

---
+++ i am poor/i am poor/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  i am poor
+//
+//  Created by Pearl Malt on 3/12/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
